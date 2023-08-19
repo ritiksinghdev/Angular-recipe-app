1c087151e922cfdb5581b66109780d01b47fb4dc
ritiksinghdev <ritik.singh@utopiatech.in>
2023-08-19 09:39:24 +0530

    added cart functionality

---
+++ src/app/shopping-list/shopping-list.service.ts
@@ -0,0 +1,56 @@
+import { Ingredient } from '../shared/ingredient.model';
+import { Subject } from 'rxjs';
+
+export class ShoppingListService {
+  ingredientsChanged = new Subject<Ingredient[]>();
+  startedEditing = new Subject<number>();
+  cartPresent = new Subject<Ingredient[]>()
+  totalAmt = new Subject<number>();
+  temp :number=0
+  private cartIngredient: Ingredient[]=[]
+  private ingredients: Ingredient[] = [
+    new Ingredient('Apples', 5),
+    new Ingredient('Tomatoes', 10),
+  ];
+
+  getIngredients() {
+    return this.ingredients.slice();
+  }
+
+  getIngredient(index: number) {
+    return this.ingredients[index];
+  }
+  addToCart(){
+    return this.cartIngredient=this.ingredients.slice()
+  }
+  addIngredient(ingredient: Ingredient) {
+    this.ingredients.push(ingredient);
+    this.ingredientsChanged.next(this.ingredients.slice());
+  }
+
+  addIngredients(ingredients: Ingredient[]) {
+    // for (let ingredient of ingredients) {
+    //   this.addIngredient(ingredient);
+    // }
+    this.ingredients.push(...ingredients);
+    this.ingredientsChanged.next(this.ingredients.slice());
+  }
+
+  updateIngredient(index: number, newIngredient: Ingredient) {
+    this.ingredients[index] = newIngredient;
+    this.ingredientsChanged.next(this.ingredients.slice());
+  }
+
+  deleteIngredient(index: number) {
+    this.ingredients.splice(index, 1);
+    this.ingredientsChanged.next(this.ingredients.slice());
+  }
+  totalAmount(){
+    this.temp=0
+    for(let i in this.ingredients){
+      this.temp+=this.ingredients[i]['amount']
+    }
+     this.totalAmt.next(this.temp)
+     return this.temp
+  }
+}
